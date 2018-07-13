1e17defe9ec7b6f4bf2b31e54ec7f10db0556eb7
ichi404gh <ichi404@yandex.ru>
2018-07-13 16:56:18 +0300

    puck delay

--- src/modules/PuckTrigger.ts
+++ src/modules/PuckTrigger.ts
@@ -1,15 +1,23 @@
-import { AbstractModule } from ".";
+import { AbstractModule } from '.';
 import { Update } from "../interfaces";
 import _const from "../const";
 import tgApi from '../services/TelegramApiService'
 
 
 export default class PuckTrigger extends AbstractModule {
+    private canPuck = true
+
     protected Filter(update: Update): boolean {
-        return update.message !== undefined && /пук/i.test(update.message.text)
+        return update.message !== undefined && /пук/i.test(update.message.text) && this.canPuck
     }
 
     protected async ProcessUpdate(update: Update) {
+        this.canPuck = false
+
+        setTimeout(() => {
+            this.canPuck = true
+        }, 1 * 60 * 1000)
+
         tgApi.SendPhoto({
             chat_id: update.message.chat.id,
             photo: _const.bubleUrl,
